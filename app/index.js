4da7ceec03feed9378c62fecc384e8b99b15b474
Nico <aroca.twelve@proton.me>
2025-07-03 12:21:34 -0400

    was able to retrieve stuff from api + single item via id!

--- app/index.js
+++ app/index.js
@@ -25,10 +25,29 @@ app.post('/api/products', async (req, res) => {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({message: error.message});
     console.error("Error!!")
   };
 });
+
+let getAllProducts = app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
+
+const getSpecificProduct = app.get('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch {
+    res.status(500).json({message: error.message});
+  }
+})
 
 mongoose.connect(`mongodb+srv://tails:${password}@backenddb.8lkktpq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`)
 .then(() => {
