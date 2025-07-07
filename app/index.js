2ed3884a85641d60e39a46fa57377912fc305974
Nico <aroca.twelve@proton.me>
2025-07-06 21:30:49 -0400

    finally updated navbar

--- app/index.js
+++ app/index.js
@@ -44,10 +44,52 @@ const getSpecificProduct = app.get('/api/product/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.status(200).json(product);
-  } catch {
+  } catch (error) {
     res.status(500).json({message: error.message});
   }
 })
+
+
+
+// update product
+
+const updateProduct = app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({message: "Product not found"})
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error){
+    res.status(500).json({message: error.message});
+  }
+})
+
+// delete product
+
+app.delete('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({message: "Product not found"})
+    }
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({message:`${product.name} deleted successfully`});
+  } catch (error){
+    res.status(500).json({message: error.message});
+  }
+})
+
+
+
+
+
+
+
+
 
 mongoose.connect(`mongodb+srv://tails:${password}@backenddb.8lkktpq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`)
 .then(() => {
